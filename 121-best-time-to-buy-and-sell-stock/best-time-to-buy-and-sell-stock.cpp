40fc75421ab74cf1b32742134e6e1ca2e6ddecf5
a6b1h6i <84183593+AbhyudayGandhi@users.noreply.github.com>
2026-08-03 16:11:47 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (25.92%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,8 +4,8 @@ public:
         int mp=0;
         int bestbuy=prices[0]; 
         for (int i=0; i<prices.size(); i++){
-            if (prices[i]-bestbuy > mp) mp=prices[i]-bestbuy;
-            if (bestbuy>prices[i]) bestbuy=prices[i];
+            mp=max(mp,prices[i]-bestbuy);
+            bestbuy=min(bestbuy,prices[i]);
         }
         return mp;
     }
