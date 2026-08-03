192e8f6f13254136248c6d2499a9e68d1a589d5d
a6b1h6i <84183593+AbhyudayGandhi@users.noreply.github.com>
2026-08-03 16:07:56 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (53.42%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int maxsum=nums[0], curr=nums[0];
-        unordered_map<char,int> mp;
         for(int i=1;i<nums.size();i++){
             curr= max(nums[i], curr+nums[i]);
             maxsum= max(curr, maxsum);
