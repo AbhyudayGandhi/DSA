d202cbfdcabc09fa9524c180221ecfe047b846ac
a6b1h6i <84183593+AbhyudayGandhi@users.noreply.github.com>
2026-09-07 00:32:13 +0530

    Time: 8 ms (24.51%) | Memory: 24.8 MB (11.35%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,8 +4,8 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> res; res.push_back(intervals[0]);
         for (auto i: intervals){
-            if(res.back()[1] < i[0]) res.push_back(i);
-            else res.back()[1]=max(res.back()[1],i[1]);
+            if (res.back()[1]<i[0]) res.push_back(i);
+            else res.back()={res.back()[0],max(i[1],res.back()[1])};
         }
 
 
